f276ca4551604090d4ca67c5f4acc41413f4e09d
ChoiGYoung <choidh1261@naver.com>
2023-06-20 16:04:54 +0900

    feat : boardview 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -33,5 +33,11 @@ public class BoardController {
 
     }
 
+    @GetMapping("/board/view")//localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
+    }
+
 
 }
